34e2292a0a3101fd8cb000a4c354cde30f92692b
Kevin <kevin@bwlohas.com>
2025-04-14 11:55:06 +0800

    init

---
+++ app/src/main/java/com/smallfat5566/airportdemo/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.smallfat5566.airportdemo.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
